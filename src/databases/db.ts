05e1022270a1085aa7f5caaed535ff79f6b98a73
Suraj Tripathi <surajtripathi.mtsl@gmail.com>
2025-07-23 23:08:28 +0530

    Update db.ts

--- src/databases/db.ts
+++ src/databases/db.ts
@@ -1,9 +1,9 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-  user: 'root',
-  host: 'localhost',
-  database: 'verceldeploy',
-  password: 'mind',
+  user: 'neondb_owner',
+  host: 'ep-empty-fire-a4foautf-pooler.us-east-1.aws.neon.tech',
+  database: 'neondb',
+  password: 'npg_BqV53KUxRapD',
   port: 5432
 });
