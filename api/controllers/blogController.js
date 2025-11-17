aa1804f98f5280222090c1ab64fc4f2cc8f0a07d
Suraj Tripathi <suraj.tripathi@classlink.com>
2025-11-17 23:53:58 +0530

    Removed api build folder

--- api/controllers/blogController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BlogController = void 0;
-exports.BlogController = {
-    create: (req, res) => {
-        res.send('main');
-    }
-};
