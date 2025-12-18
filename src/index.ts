5aad123bc43f77e1412e7c3d4ac09ca340c7884b
Suraj Tripathi <suraj.tripathi@classlink.com>
2025-12-18 15:40:00 +0530

    swagger fix

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
-import path from 'path';
 import express from 'express'
 import bodyParser from "body-parser";
 const app = express();
-import { setupSwagger } from './swagger';
-
 
 import userRouter from './routes/users';
 import blogRouter from './routes/blogs';
+import homeRouter from './routes/home';
 
 process.on('uncaughtException', (err) => {
   console.log(err);
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api-docs',express.static(path.join(__dirname, 'public/swagger-ui')))
-setupSwagger(app);
 app.use(bodyParser.urlencoded({
   limit: '20mb',
   extended: true
@@ -24,11 +19,14 @@ app.use(bodyParser.json({
   limit: '20mb'
 }));
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
 
 /** Router Block */
+app.use('/',homeRouter)
 app.use('/users', userRouter);
 app.use('/blogs', blogRouter);
-
 
 /** End Router Block */
 
