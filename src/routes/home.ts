4f54901ed3b85ff6905bfaa6e225c33c252733cd
Suraj Tripathi <suraj.tripathi@classlink.com>
2025-12-19 10:10:36 +0530

    Swagger Update

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,9 +1,30 @@
 import express from 'express'
+import path from 'path';
+import fs from 'fs';
 
-const router= express.Router();
+const router = express.Router();
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.render('swagger');
+});
+
+router.get('/openapi.json', (req, res) => {
+    const filePath = path.join(__dirname,'./../public/swagger.json');
+    // Read JSON file asynchronously
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err.message);
+            return res.status(500).json({ error: 'Failed to read JSON file' });
+        }
+
+        try {
+            const jsonData = JSON.parse(data); // Parse JSON safely
+            res.json(jsonData); // Send JSON response
+        } catch (parseErr: any) {
+            console.error('Error parsing JSON:', parseErr.message);
+            res.status(500).json({ error: 'Invalid JSON format' });
+        }
+    });
 });
 
 
