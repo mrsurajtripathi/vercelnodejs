af64fab9f206b299414f3369748ee31aa39f5b28
Suraj Tripathi <suraj.tripathi@classlink.com>
2025-12-20 13:37:38 +0530

    upload folder issue

--- src/utils/csvUploads.ts
+++ src/utils/csvUploads.ts
@@ -1,2 +1,7 @@
 import multer from 'multer';
-export default multer({ dest: 'uploads/' });
+export default multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 10 * 1024 * 1024, // 10MB example
+    },
+});
