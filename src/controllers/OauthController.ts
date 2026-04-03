c1cd2bb699509fc07f3c2e3894137cd2e4f4b237
Suraj Tripathi <suraj.tripathi@classlink.com>
2026-04-03 09:19:30 +0530

    oauth token endpoint

--- src/controllers/OauthController.ts
+++ src/controllers/OauthController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
 import { pool } from '../databases/db';
 import { v4 as uuid4 } from 'uuid';
+import { createHash } from 'crypto';
+import { generateCodeChallenge } from '../utils/pkce';
+import { sign } from 'jsonwebtoken';
 
 export const oauthController = {
     create: async (req: Request, res: Response) => {
@@ -41,11 +44,82 @@ export const oauthController = {
         }
         const code = uuid4();
         await pool.query(
-            `INSERT INTO oauth_auth_codes
+            `INSERT INTO authorization_codes
     (code, code_challenge, code_challenge_method, expires_at, client_id, user_id)
-    VALUES ($1,$2,$3,NOW() + interval '10 minutes',$4,$5)`,
-            [code, code_challenge, code_challenge_method, client_id, 1]
+    VALUES ($1,$2,$3,$4,$5,$6)`,
+            [code, code_challenge, code_challenge_method, Date.now() + (60 * 60 * 1000), client_id, 1]
         );
-        return res.send({ status: 1, redirecturi: `${redirect_uri}?code=${code}&state=${state}` });
+
+        let direct_uri = client.redirect_uri;
+        direct_uri += `?code=${code}`;
+        if (state) {
+            direct_uri += `&state=${state}`;
+        }
+        return res.send({ status: 1, redirecturi: `${direct_uri}` });
+    },
+    token: async (req: Request, res: Response) => {
+        const {
+            code,
+            client_id,
+            code_verifier,
+            client_secret
+        } = req.body;
+
+        if (!code || !client_id || !client_secret) {
+            res.send({ status: 0, error: 'Invalid Parameter' });
+            return;
+        }
+        const { rows } = await pool.query(
+            `SELECT * FROM authorization_codes WHERE code = $1 AND client_id = $2`,
+            [code, client_id]
+        );
+
+        if (rows.length === 0) {
+            res.send({ status: 0, error: 'Invalid Code' });
+            return;
+        }
+        const authCode = rows[0];
+
+        let clientrow = await pool.query(
+            `SELECT * FROM oauth_clients WHERE client_id = $1 AND client_secret = $2`,
+            [client_id, client_secret]
+        );
+
+        if(clientrow.rows.length === 0){
+            res.send({ status: 0, error: 'Invalid Client Credentials' });
+            return;
+        }
+
+        /*const hash = generateCodeChallenge(code_verifier); //createHash('sha256').update(code_verifier).digest('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+        if (hash !== authCode.code_challenge) {
+            return res.status(400).json({ error: 'PKCE verification failed' });
+        }*/
+
+        // Generate tokens
+        const accessToken = sign(
+            { user_id: authCode.user_id },
+            'SECRET',
+            { expiresIn: '1h' }
+        );
+
+        const refreshToken = uuid4();
+        await pool.query(
+            `INSERT INTO oauth_tokens
+    (access_token, refresh_token, expires_at, client_id, user_id)
+    VALUES ($1,$2,NOW()+interval '1 hour',$3,$4)`,
+            [accessToken, refreshToken, client_id, authCode.user_id]
+        );
+
+        await pool.query(
+            'DELETE FROM oauth_auth_codes WHERE code=$1',
+            [code]
+        );
+
+        res.json({
+            access_token: accessToken,
+            refresh_token: refreshToken,
+            token_type: 'Bearer',
+            expires_in: 3600
+        });
     }
 }
--- src/routes/oauth.ts
+++ src/routes/oauth.ts
@@ -7,4 +7,5 @@ router.get('/authorize',oauthController.authorize);
 // https://auth.surajtripathi.com/oauth/authorize?response_type=code&client_id=a160c673-da31-4c75-af01-c95fd71d16d1&redirect_uri=https://example.com/callback&scope=read&state=xyz
 //GET https://auth.surajtripathi.com/oauth/authorize?client_id=abc123&redirect_uri=http://localhost:5173/callback&response_type=code&code_challenge=XYZ&code_challenge_method=S256&state=abc
 
+router.post('/token',oauthController.token);
 export default router;
